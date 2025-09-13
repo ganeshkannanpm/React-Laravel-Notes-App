7b7e39c1dd859fd16a5b17ad97cfd4f57cece679
Ganesh Kannan <ganeshkannan995gk@gmail.com>
2025-09-13 14:43:37 +0530

    feat: implemented create note process

--- resources/src/components/CreateNote.jsx
+++ resources/src/components/CreateNote.jsx
@@ -1,16 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const CreateNote = () => {
+    const [isExpanded, setExpanded] = useState(false);
 
-  return (
-    <div>
-        <form className='create-note'>
-            <input type="text" name='title' placeholder='Title' />
-            <input type="text" name='content' placeholder='Take a note...' />
-            <button>Add</button>
-        </form>
-    </div>
-  )
-}
+    const [note, setNote] = useState({
+        title: "",
+        content: "",
+    });
 
-export default CreateNote
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setNote((prevNote) => {
+            return {
+                ...prevNote,
+                [name]: value,
+            };
+        });
+    };
+
+    const submitNote = (e) => {
+        e.preventDefault();
+        //Backend
+
+        setNote({
+            title: "",
+            content: "",
+        });
+    };
+
+    const expand = () => {
+        setExpanded(true);
+    };
+
+    return (
+        <div>
+            <form className="create-note">
+                {isExpanded ? (
+                    <input
+                        type="text"
+                        onChange={handleChange}
+                        name="title"
+                        value={note.title}
+                        placeholder="Title"
+                    />
+                ) : null}
+
+                <textarea
+                    onClick={expand}
+                    onChange={handleChange}
+                    name="content"
+                    value={note.content}
+                    placeholder="Take a note..."
+                    rows={isExpanded ? 3 : 1}
+                ></textarea>
+                <button onClick={submitNote}>Add</button>
+            </form>
+        </div>
+    );
+};
+
+export default CreateNote;
