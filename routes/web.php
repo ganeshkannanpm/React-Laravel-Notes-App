6a6a32c6e61b12b8a0711be14a4982bf9a66cf63
Ganesh Kannan <ganeshkannan995gk@gmail.com>
2025-09-13 12:50:51 +0530

    feat: Setup login redirection and welcome blade updates

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Catch-all route for React Router
+Route::view('/{any}', 'welcome')->where('any', '.*');
